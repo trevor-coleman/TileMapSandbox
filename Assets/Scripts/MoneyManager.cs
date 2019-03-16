98b11be3f1278751d06e3e3ddef101525058bebd
Trevor Coleman <trevor@trevorcoleman.design>
2019-03-16 18:12:39 -0400

    implement MoneyManager

---
+++ Assets/Scripts/MoneyManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoneyManager : MonoBehaviour
+{
+
+    private int bankBalance;
+    public int BankBalance => bankBalance; 
+    [SerializeField] private int startingBankBalance;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        bankBalance = startingBankBalance;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public bool CanSpend(int amount)
+    {
+        return bankBalance > amount;
+    }
+
+    public bool Spend(int amount)
+    {
+        if (CanSpend(amount))
+        {
+            bankBalance -= amount;
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+        
+    }
+
+    public void Deposit(int amount)
+    {
+        bankBalance += amount;
+    }
+}
